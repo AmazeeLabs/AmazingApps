6a3b2ec145b8ba83e4d48bdfd1ab9c26d96c3547
Philipp Melab <philipp.melab@gmail.com>
2019-03-22 13:28:55 +0100

    Naive implementation.

--- storybook/twig/02-atoms/collapsible/collapsible.ts
+++ storybook/twig/02-atoms/collapsible/collapsible.ts
@@ -1,7 +1,25 @@
 class Collapsible extends HTMLElement {
     constructor() {
         super();
-        console.log('Creating element!');
+        let collapsed = true;
+
+        const contentWrapper = this.ownerDocument.createElement('div');
+        contentWrapper.style.display = 'none';
+        this.append(contentWrapper);
+
+        while (this.firstChild !== contentWrapper) {
+            contentWrapper.appendChild(this.firstChild);
+        }
+
+        const toggle = this.ownerDocument.createElement('button');
+        toggle.innerText = 'Open';
+        this.prepend(toggle);
+
+        toggle.addEventListener('click', () => {
+            collapsed = !collapsed;
+            toggle.innerText = collapsed ? 'Open' : 'Close';
+            contentWrapper.style.display = collapsed ? 'none' : 'block';
+        });
     }
 }
 
